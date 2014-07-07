cf298b1aceb3b7b1219133f696a48eeac24821f4
Yukariko <rkdals7979@naver.com>
2014-07-07 18:06:22 +0900

    2644

---
+++ problem/2644/test.c
@@ -0,0 +1,17 @@
+a[101][101];x;y;n;
+f(p,i,c)
+{
+	for(;i<=n;i++)
+		if(a[i][p] || a[p][i])
+		{
+			a[i][p]=a[p][i]=0;
+			f(i,1,c+1);
+		}
+	if (p == y){printf("%d",c);exit(1);}
+}
+main(p,q)
+{
+	for(scanf("%d%d%d%d",&n,&x,&y,&t);~scanf("%d%d",&p,&q);a[p][q]++);
+	f(x,1,0);
+	printf("-1");
+}
