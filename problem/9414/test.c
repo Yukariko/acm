0b43a078e14c635539936601c35080fe987baa89
Yukariko <rkdals7979@naver.com>
2014-07-07 21:37:05 +0900

    9414

---
+++ problem/9414/test.c
@@ -0,0 +1,18 @@
+s,n,z,i,j,k=5000001,a[11];
+main(t){
+  for(scanf("%d",&t);t--;)
+  {
+    for(z=0;scanf("%d",a+z),a[z];z++);
+    for(i=0;i<z;i++)
+      for(j=i;j>=0;j--)
+        if(a[j]<a[j+1])
+        {
+          n=a[j];
+          a[j]=a[j+1];
+          a[j+1]=n;
+        }
+    for(s=i=0;i<z&&s<k;i++)
+      s+=2*powl(a[i],i+1);
+    s<k?printf("%d\n",s):puts("Too expensive");
+  }
+}
