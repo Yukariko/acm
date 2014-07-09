6b538728408c3207cfd84355508ef4bb2d112f61
Yukariko <rkdals7979@naver.com>
2014-07-09 17:26:44 +0900

    7894
    easy to understand

--- problem/7894/test.c
+++ problem/7894/test.c
@@ -1,1 +1,1 @@
-long double s;main(n){for(gets(&n);~scanf("%d",&n);printf("%d\n",s?(int)(ceill(s)+1e-6):1))for(s=0;n;n--)s+=log10l(n);}
+double s;main(n){for(gets(&n);~scanf("%d",&n);printf("%d\n",s?(int)ceil(s):1))for(s=0;n;n--)s+=log10(n);}
