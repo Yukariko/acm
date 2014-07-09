aedd81671851ed6c1a75bacff18a65e3246a1e62
yukariko <rkdals7979@naver.com>
2014-07-09 20:19:22 +0900

    1629
    solution

--- problem/1629/test.c
+++ problem/1629/test.c
@@ -1,12 +1,12 @@
-long long A,B,C,R=1,D;a[1001];j;
+long long A,B,C;
+f(long long a,long long b)
+{
+  if(b==0)return 1;
+  if(b==1)return a%C;
+  if(b%2==0)return f((a*a)%C,b/2);
+  else return (a*f((a*a)%C,(b-1)/2))%C;
+}
 main(i){
-  for(scanf("%lld%lld%lld",&A,&B,&C),A%=C;B;B--)
-  {
-    R=(R*A)%C;
-    for(j=1;j<i;j++)if (a[j]==R){break;} 
-    if(j!=i)break;
-    a[i++]=R;
- //   printf("%d %lld\n",i-1,R);
-  }
-  printf("%lld",B%(i-j)?a[B%(i-j)]:R);
+  scanf("%lld%lld%lld",&A,&B,&C);
+  printf("%d",f(A,B));
 }
