b0c1d72e42e3f917403f67601ba57d8aceca8abd
Yukariko <rkdals7979@naver.com>
2014-07-10 14:58:23 +0900

    2485
    more efficient

--- problem/2485/test.c
+++ problem/2485/test.c
@@ -3,8 +3,7 @@ gcd(a,b){return b?gcd(b,a%b):a;}
 main(n)
 {
   for(scanf("%d",&n);i<n;i++)scanf("%d",a+i);
-  min=987654321;
-  for(i=1;i<n;i++)min=min>a[i]-a[i-1]?a[i]-a[i-1]:min;
+  min=a[1]-a[0];
   for(i=1;i<n;i++)min=gcd(min,a[i]-a[i-1]);
   for(i=1;i<n;i++)s+=(a[i]-a[i-1]-min)/min;
   printf("%d",s);
