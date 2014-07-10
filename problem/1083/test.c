d82c67a5d247a8cc2b2c7bcb1eac2973d6812504
Yukariko <rkdals7979@naver.com>
2014-07-11 00:59:33 +0900

    1083

--- problem/1083/test.c
+++ problem/1083/test.c
@@ -1,10 +1,13 @@
-int a[51],i,j,t,m,p;
+int a[51],i,j,t,m,k,p;
 main(n,s){
-    for(scanf("%d",&n);i<n;i++)scanf("%d",a+i);scanf("%d",&s);
+    while(~scanf("%d",&n)){
+    for(i=0;i<n;scanf("%d",a+i++));scanf("%d",&s);
     for(i=0;i<n&&s>0;i++)
     {
-      p=m=0;for(j=i;j<n&&j<=i+s;j++)if(m<=a[j]){m=a[j];p=j;}
+      p=m=-1;for(j=i,k=0;j<n&&k<=s;j++,k++)if(m<a[j]){m=a[j];p=j;}
       for(j=p;j>i&&s;j--){t=a[j];a[j]=a[j-1];a[j-1]=t;s--;}
     }
     for(i=0;i<n;i++)printf("%d ",a[i]);
+    puts("");
+    }
 }
