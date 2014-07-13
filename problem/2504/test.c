a32d58f2869cbc8fb441c977808241959e93d8db
Yukariko <rkdals7979@naver.com>
2014-07-13 22:16:08 +0900

    2504
    yo-ho!

--- problem/2504/test.c
+++ problem/2504/test.c
@@ -1,15 +1,7 @@
-struct score{long long val,index;}v[100];
-char a[61];
-int check[61];
-int point[61];
-int i,j,c;
-long long p,s;
-find(long long n)
-{
-  int t;
-  if(n)for(t=0;t<100;t++)if(v[t].val==n)return t;
-  return -1;
-}
+char a[31];
+int check[31];
+int point[31];
+int i,j;
 main()
 {
   for(gets(a);a[i];i++)
@@ -17,29 +9,13 @@ main()
     if(a[i]=='('||a[i]=='[')
     {
       check[j++]=a[i]=='('?')':']';
-      point[j]=1;
     }
     if(a[i]==')'||a[i]==']')
     {
-      point[j-1]+=(check[j-1]==')'?2:3)*point[j];
-      --j;
-      if(j<0||check[j]!=a[i]){c=1;break;}
+      --j;if(j<0||check[j]!=a[i]){point[0]=0;break;}
+      point[j]+=(check[j]==')'?2:3)*(point[j+1]?point[j+1]:1);
+      point[j+1]=0;
     }
   }
-  printf("%d\n",point[0]);
-  while(1);
-  if(!c&&!j)
-  {
-    v[0].val=p=1;
-    for(i=0;a[i];i++)
-    {
-      if(a[i]=='('){p*=2;if(find(p)==-1)v[i+1].val=p;}
-      if(a[i]=='['){p*=3;if(find(p)==-1)v[i+1].val=p;}
-      if(a[i]==')'){c=find(p/2);v[c].index-=1;c=find(p);v[c].index+=1;v[c].val=p;p/=2;}
-      if(a[i]==']'){c=find(p/3);v[c].index-=1;c=find(p);v[c].index+=1;v[c].val=p;p/=3;}
-    }
-     
-    for(i=0;i<100;i++)s+=v[i].index>0?v[i].val*v[i].index:0;
-  }
-  printf("%lld",s);
+  printf("%d",point[0]);
 }
