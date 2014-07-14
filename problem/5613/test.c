b19d8a36f75f9e5a8cc043645ac302c770ddebea
yukariko <rkdals7979@naver.com>
2014-07-14 22:39:08 +0900

    5613

--- problem/5613/test.c
+++ problem/5613/test.c
@@ -1,1 +1,1 @@
-char a[9];s;k;main(n){for(s=atoi(gets(a));~scanf(" %c %d ",&k,&n);s=k=='+'?s+n:k=='-'?s-n:k=='/'?s/n:k=='*'?s*n:s);printf("%d",s);}
+s;k;main(n){for(scanf("%d",&s);~scanf(" %c%d",&k,&n);s=k=='+'?s+n:k=='-'?s-n:k=='/'?s/n:k=='*'?s*n:s);printf("%d",s);}
