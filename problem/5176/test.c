455a36ce2242ce731539136c2bab14e0feb8f293
Yukariko <rkdals7979@naver.com>
2014-07-15 18:24:51 +0900

    5176

---
+++ problem/5176/test.c
@@ -0,0 +1,11 @@
+int p,m,n,s,i;
+main(k)
+{
+  for(scanf("%d",&k);k--;printf("%d\n",p-s))
+  {
+    int seat[501]={0};
+    scanf("%d%d",&p,&m);
+    for(i=0;i++<p;seat[n]++)scanf("%d",&n);
+    for(s=i=0;i<=m;i++)s+=seat[i]?1:0;
+  }
+}
