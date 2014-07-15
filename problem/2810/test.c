5afa0c1bc2d987f863c58a46ae6869ff1297439e
yukariko <rkdals7979@naver.com>
2014-07-16 08:06:18 +0900

    2810

--- problem/2810/test.c
+++ problem/2810/test.c
@@ -1,6 +1,6 @@
 int i,s;
 main(n)
 {
-  for(scanf("%d",&n);scanf(" %c",&i);)s+=i=='L'?1:0;
-  printf("%d",n-s/2+1);
+  for(scanf("%d",&n);~scanf(" %c",&i);)s+=i=='L'?1:0;
+  printf("%d",n-(s?s/2-1:0));
 }
