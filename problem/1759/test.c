a676fc2c7cf3417c8a51f9142b888b1c84d89fcd
Yukariko <rkdals7979@naver.com>
2014-07-18 16:22:55 +0900

    1759

---
+++ problem/1759/test.c
@@ -0,0 +1,35 @@
+char crypt[16],t;
+char print[16];
+int i,j,L,C;
+f(i,c)
+{
+  char temp;
+  if(c==L)
+  {
+    int vowel=0,consonant=0;
+    for(i=0;i<L;i++)
+    {
+      if(print[i]=='a'||print[i]=='e'||print[i]=='i'||print[i]=='o'||print[i]=='u')vowel++;
+      else consonant++;
+    }
+    if(vowel>0&&consonant>1)puts(print);
+    return;
+  }
+  for(;i<C;i++)
+  {
+    if(crypt[i])
+    {
+      temp=crypt[i];
+      crypt[i]=0;
+      print[c]=temp;
+      f(i,c+1);
+      crypt[i]=temp;
+    }
+  }
+}
+main()
+{
+  for(scanf("%d%d",&L,&C);i<C;i++)scanf(" %c",crypt+i);
+  for(i=0;i<C;i++)for(j=i;j>0;j--)if(crypt[j]<crypt[j-1]){t=crypt[j-1];crypt[j-1]=crypt[j];crypt[j]=t;}
+  f(0,0);
+}
