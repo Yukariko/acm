4b1d0d0e4a138c70021b4561a21b86181e6068e9
Yukariko <rkdals7979@naver.com>
2014-08-08 07:04:25 +0900

    1872
    success!!

--- problem/1872/test.c
+++ problem/1872/test.c
@@ -1,4 +1,3 @@
-char a[10001];
 int rome[128];
 char dim[]="mdclxvi";
 init()
@@ -8,36 +7,40 @@ init()
   rome['c']=100;rome['d']=500;
   rome['m']=1000;
 }
-long long rome2dim(char *b)
-{
-  int i;
-  long long s;
-  for(s=i=0;b[i];i++)
-  {
-    s+=rome[b[i]];
-  }
-  return s;
-}
-
 main(m,k,j,i,n)
 {
   init();
   for(scanf("%d ",&n);n--;)
   {
+    char a[10001];
     gets(a);
-    char b[10001]={0};
     int count[7]={10000,1,3,1,3,1,3};
+    long long s=0;
     for(m=j=k=0;k<7;k++)
     for(i=m;a[i];i++)
     {
-      if(a[i]==dim[k]&&count[k])
+      if(a[i]==dim[k]&&count[k]>0)
       {
         count[k]--;
-        b[j++]=a[i];
+        s+=rome[a[i]];
         m=i;
       }
+      else if(a[i]==dim[k]&&count[k]==0)
+      {
+        if(k==2||k==4)
+        {
+          int t;
+          for(t=i;t>m;t--)if(a[t]==dim[k+2])break;
+          if(t!=m)
+          {
+            s+=rome[a[i]]-rome[a[t]];
+            m=i;
+            count[k]--;
+            count[k+1]=count[k+2]=0;
+          }
+        }
+      }
     }
-    b[j]=0;
-    printf("%lld\n",rome2dim(b));
+    printf("%lld\n",s);
   }
 }
