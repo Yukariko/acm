f213d836331b29c3e2f31935f6d45468444bbae5
Yukariko <rkdals7979@naver.com>
2014-08-08 12:26:59 +0900

    1793

---
+++ problem/1793/test.c
@@ -0,0 +1,34 @@
+int n;
+bigplus(char *store,char *a,char *b)
+{
+  char t[100]={0};
+  int i=strlen(a),j=strlen(b),p,s;
+  for(p=0;i||j;p++)
+  {
+    s = (i?a[i-1]:'0')+(j?b[j-1]:'0')-'0'*2;
+    if(s+t[p]>9)t[p+1]+=(s+t[p])/10;
+    t[p] = (s+t[p])%10;
+    if(i>0)i--;
+    if(j>0)j--;
+  }
+  if(!t[p])p--;
+  for(i=0;i<=p;i++)
+   store[p-i]=t[i]+'0';
+  store[p+1]=0;
+}
+main()
+{
+  for(;~scanf("%d",&n);)
+  {
+    char i[100]={0},j[100]={0},t[100]={0};
+    strcpy(i,"0");strcpy(j,"1");
+    for(;n--;)
+    {
+      strcpy(t,j);
+      bigplus(j,j,i);
+      bigplus(j,j,i);
+      strcpy(i,t);
+    }
+    printf("%s\n",j);
+  }
+}
