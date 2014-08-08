378e68c271c3fc79f708f55aaeeaae645b49f33e
Yukariko <rkdals7979@naver.com>
2014-08-08 10:19:45 +0900

    9372

---
+++ problem/9372/test.c
@@ -0,0 +1,16 @@
+int t;
+int n,m;
+int a,b;
+int i,j;
+main()
+{
+  for(scanf("%d",&t);t--;)
+  {
+    scanf("%d%d",&n,&m);
+    for(i=0;i<m;i++)
+    {
+      scanf("%d%d",&a,&b);
+    }
+    printf("%d\n",n-1);
+  }
+}
