88f2afbf27b13ca769268da9bc646740c4864c15
Yukariko <rkdals7979@naver.com>
2014-08-10 00:51:16 +0900

    7806

--- problem/7806/test.c
+++ problem/7806/test.c
@@ -1,32 +1,26 @@
 main()
 {
-  int p[31623];
+  int p;
   int a,b;
   long long i,j,k,s,t;
   for(;~scanf("%d%d",&a,&b);)
   {
-    t=sqrt(b)+1e-6;
-    k=0;
-    for(i=2;i<=t;i++)
+    t=b;
+    s=1;
+    for(i=2;i*i<=t;i++)
     {
-      p[i]=0;
+      p=0;
       for(;b%i==0;)
       {
         b/=i;
-        p[i]+=1;
-        k=i;
+        p+=1;
       }
-      if(b<2)break;
-    }
-    t=k;
-    s=1;
-    for(i=2;i<=t;i++)
-    {
-      if(p[i])
+      if(p)
       {
         k=0;for(j=i;j<=a;j*=i)k+=a/j;
-        for(j=p[i]<k?p[i]:k;j--;)s*=i;
+        for(j=p<k?p:k;j--;)s*=i;
       }
+      if(b<2)break;
     }
     if(b>1&&b<=a)s*=b;
     printf("%d\n",s);
