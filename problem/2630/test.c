0c48c922765d37957c4ed1e14c91d6680b023fee
Yukariko <rkdals7979@naver.com>
2014-08-11 00:03:52 +0900

    2630

--- problem/2630/test.c
+++ problem/2630/test.c
@@ -1,6 +1,7 @@
 int arr[129][129];
 int n;
-int count;
+int white;
+int blue;
 int bool;
 split(p,q,r)
 {
@@ -9,16 +10,14 @@ split(p,q,r)
   {
     for(j=0;j<r;j++)
     {
-      if(arr[p+i][q+j]!=bool)
+      if(arr[p+i][q+j])
       {
-        off=1;
-        break;
+        off++;
       }
     }
-    if(off)break;
   }
-  if(!off){count++;return;}
-  if(r==1){return;}
+  if(!off){white++;return;}
+  if(off == r*r){blue++;return;}
   split(p,q,r/2);
   split(p+r/2,q,r/2);
   split(p,q+r/2,r/2);
@@ -30,8 +29,5 @@ main()
   scanf("%d",&n);
   for(i=0;i<n;i++)for(j=0;j<n;j++)scanf("%d",&arr[i][j]);
   split(0,0,n);
-  c1=count;count=0;
-  bool=!bool;
-  split(0,0,n);
-  printf("%d\n%d",c1,count);
+  printf("%d\n%d",white,blue);
 }
