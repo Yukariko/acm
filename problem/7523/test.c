0934331e6dd065cc642f79e62ec6284a6a84a19b
Yukariko <rkdals7979@naver.com>
2014-08-12 15:34:51 +0900

    7523

---
+++ problem/7523/test.c
@@ -0,0 +1,29 @@
+int t;
+long long n,m;
+long long s;
+long long absl(long long a)
+{
+  return a>0?a:-a;
+}
+main()
+{
+  int i;
+  scanf("%d",&t);
+  for(i=1;i<=t;i++)
+  {
+    scanf("%lld%lld",&n,&m);
+    printf("Scenario #%d:\n",i);
+
+    if(n*m>=0)
+    {
+      if(n>m)s=n*(n+1)/2-m*(m-1)/2;
+      else s=m*(m+1)/2-n*(n-1)/2;
+    }
+    else
+    {
+      if(n<0)s=-n*(n-1)/2 + m*(m+1)/2;
+      else s=n*(n+1)/2 - m*(m-1)/2;
+    }
+    printf("%lld\n\n",s);
+  }
+}
