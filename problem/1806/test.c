7c26bce6fa6864b004cab24b176772d873c0dc05
Yukariko <rkdals7979@naver.com>
2014-08-16 07:22:21 +0900

    1806
    12ms

--- problem/1806/test.c
+++ problem/1806/test.c
@@ -1,30 +1,26 @@
 int n,s;
-int arr[100001];
-int sort[100001];
-int window;
-int max;
-cmp(int *a,int *b){return *a>*b?-1:*a<*b?1:0;}
 main()
 {
+  int arr[100001];
   scanf("%d%d",&n,&s);
-  int i,j;
-  for(i=0;i<n;sort[i]=arr[i],i++)scanf("%d",arr+i);
-  qsort(sort,n,4,cmp);
-  for(window=i=0;i<n;i++)
+  int i,j,k,sum,min=n+1;
+  for(i=0;i<n;scanf("%d",arr+i++));
+  for(sum=k=i=0;i<n;i++)
   {
-    window+=sort[i];
-    if(window>=s)break;
-  }
-
-  for(++i;i<=n;i++)
-  {
-    window=0;
-    for(j=0;j<i;j++)window+=arr[j];
-    for(;j<n&&window<s;j++)
+    sum+=arr[i];
+    if(sum>=s)
     {
-      window=window-arr[j-i]+arr[j];
+      for(;k<=i;k++)
+      {
+        sum-=arr[k];
+        if(sum<s)
+        {
+          sum+=arr[k];
+          break;
+        }
+      }
+      min=min>i-k?i-k:min;
     }
-    if(window>=s){printf("%d",i);return;}
   }
-  puts("0");
+  printf("%d",min==n+1?0:min+1);
 }
