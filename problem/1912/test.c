8ff6ab71ce7faeb60941e163c340ea7dcadb82ac
Yukariko <rkdals7979@naver.com>
2014-08-16 09:38:08 +0900

    1912

--- problem/1912/test.c
+++ problem/1912/test.c
@@ -1,29 +1,13 @@
-int n,s;
-int arr[100001];
-int sort[100001];
-int window;
-int max;
-cmp(int *a,int *b){return *a>*b?-1:*a<*b?1:0;}
 main()
 {
-  scanf("%d%d",&n,&s);
-  int i,j;
-  for(i=0;i<n;sort[i]=arr[i],i++)scanf("%d",arr+i);
-  qsort(sort,n,4,cmp);
-  for(window=i=0;i<n;i++)
+  int n,i,k,s,m=1<<31;
+  scanf("%d",&n);
+  for(s=i=0;i<n;i++)
   {
-    window+=sort[i];
-    if(window>=s)break;
+    scanf("%d",&k);
+    if(s<0)s=k;
+    else s+=k;
+    m=m<s?s:m;
   }
-  for(++i;i<=n;i++)
-  {
-    window=0;
-    for(j=0;j<i;j++)window+=arr[j];
-    for(;j<n&&window<s;j++)
-    {
-      window=window-arr[j-i]+arr[j];
-    }
-    if(window>=s){printf("%d",i);return;}
-  }
-  puts("0");
+  printf("%d",m);
 }
