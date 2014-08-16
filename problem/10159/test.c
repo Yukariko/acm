078bf8c054b1bd9258f6d1bd29eca64f6f6189d3
Yukariko <rkdals7979@naver.com>
2014-08-17 01:52:57 +0900

    10159
    not yet

---
+++ problem/10159/test.c
@@ -0,0 +1,44 @@
+int a[101][101];
+int visit[101][101];
+int n,m;
+int c;
+f(x,y,v)
+{
+  int i,j;
+  c++;
+  for(i=1;i<=n;i++)
+  {
+    if(visit[x][i]==0&&a[x][i]==v)
+    {
+      a[y][i]=v;
+      f(i,y,a[x][i]);
+    }
+  }
+}
+main()
+{
+  int x,y;
+
+  for(scanf("%d%d",&n,&m);m--;)
+  {
+    scanf("%d%d",&x,&y);
+    a[x][y]=-1;
+    a[y][x]=1;
+  }
+  int i,j,s;
+  for(i=1;i<=n;i++)
+  {
+    f(i,i,1);
+    f(i,i,-1);
+  }
+  for(i=1;i<=n;i++)
+  {
+    s=n-1;
+    for(j=1;j<=n;j++)
+    {
+      if(a[i][j])s--;
+    }
+    printf("%d\n",s);
+  }
+  printf("[%d]", 1/0);
+}
