19628f5a9eb840a4f7a35ab3b311881cd22acd6b
Yukariko <rkdals7979@naver.com>
2014-08-18 12:37:18 +0900

    1753

--- problem/1753/test.c
+++ problem/1753/test.c
@@ -1,97 +1,81 @@
-#define MAX_QUEUE 20001
-typedef struct{int start;int end;int m;} DATA;
-typedef struct queue{DATA data;struct queue *next;}queue;
-queue *head[MAX_QUEUE],*tail[MAX_QUEUE];
+#define MAX_STACK 20001
+typedef struct{int v;int w;} DATA;
+typedef struct stack{DATA data;struct stack *next;}stack;
+stack *head[MAX_STACK];
 void push(int index,DATA val)
 {
-  tail[index]->data=val;
-  tail[index]->next=(queue *)malloc(sizeof(queue));
-  tail[index]=tail[index]->next;
-  memset(&tail[index]->data,0,sizeof(DATA));
-  tail[index]->next=0;
+  stack *p=head[index];
+  head[index]=(stack *)malloc(sizeof(stack));
+  head[index]->data=val;
+  head[index]->next=p;
 }
-DATA pop(int index)
-{
-  queue *p=head[index];
-  DATA result=head[index]->data;
-  head[index]=head[index]->next;
-  free(p);
-  return result;
-}
-int isEmpty(int index)
-{
-  return !head[index];
-}
+
 void init(int index)
 {
-  head[index]=(queue *)malloc(sizeof(queue));
-  memset(&head[index]->data,0,sizeof(DATA));
+  head[index]=(stack *)malloc(sizeof(stack));
   head[index]->next=0;
-  tail[index]=head[index];
 }
-void destroy(int index)
+
+int d[20001];
+int check[20001];
+int v,e,k,s;
+extraMin()
 {
-  for(;!isEmpty(index);)pop(index);
-}
-int find(int index,int val)
-{
-  queue *p=head[index];
-  for(;p;p=p->next)if(p->data.end==val)return p->data.m;
-  return -1;
-}
-int visited[20001];
-int N,M,K;
-int i;
-prim()
-{
-  int i,j,s,m;
-  int x,y;
-  int result=0;
-  DATA t;
-  queue *p,*q;
-  init(0);
-  visited[K]=1;
-  t.start = k;
-  t.end = K;
-  t.m = 0;
-  s=1;
-  push(0,t);
-  for(;s<N;s++)
+  int i,min=65535,res=0,j=0,t=0,k;
+  for(i=1;i<=s;++i)
   {
-    m=98765;
-    for(p=head[0];p;p=p->next)
+    k=check[i];
+    if(head[k])
     {
-      j=p->data.end;
-      for(q=head[j];q;q=q->next)
+      if(d[k]<min)
       {
-        i=q->data.end;
-        if(!visited[i]&&m>q->data.m)
-        {
-          m=q->data.m;
-          x=i;
-          y=j;
-        }
+        min=d[k];
+        res=k;
+        t=i;
       }
     }
-    t.start=y;
-    t.end=x;
-    t.m=m;
-    visited[x]=1;
-    push(0,t);
+    else if(++j>=s)break;
   }
-  for(i=1;i<=N;i++)
-  {
-    m=find(0,i);
-    if(m>=0)printf("%d\n",m);
-    else puts("INF");
-  }
-  return result;
+  if(t){check[t]=check[s];}
+  s--;
+  return res;
 }
-main(x,y,m)
+dijkstra()
 {
-  scanf("%d%d%d",&N,&M,&K);
+  int i,j;
+  int u;
+  for(i=1;i<=v;++i)d[i]=65535;
+  d[k]=0;
+  for(s=v;s;)
+  {
+    u=extraMin();
+    stack *p;
+    for(p=head[u];p;p=p->next)
+    {
+      if(head[p->data.v]&&d[p->data.v]>d[u]+p->data.w)
+      {
+        d[p->data.v]=d[u]+p->data.w;
+      }
+    }
+    head[u]=0;
+  }
+  for(i=1;i<=v;++i)
+  {
+    if(d[i]==65535)puts("INF");
+    else printf("%d\n",d[i]);
+  }
+}
+main()
+{
+
+  int i,j;
+  scanf("%d%d%d",&v,&e,&k);
   DATA t;
-  for(i=1;i<=N;i++)init(i);
-  for(i=1;i<=M;i++){scanf("%d%d%d",&x,&y,&m);t.start=x;t.end=y;t.m=m;push(x,t);}
-  prim();
+  for(i=1;i<=v;i++){init(i);check[i]=i;}
+  for(i=0;i<e;i++)
+  {
+    scanf("%d%d%d",&j,&t.v,&t.w);
+    push(j,t);
+  }
+  dijkstra();
 }
