0e14b83513dcd31256f7b29118448a1bfd3ab801
Yukariko <rkdals7979@naver.com>
2014-08-19 15:54:49 +0900

    1343

---
+++ problem/1343/test.c
@@ -0,0 +1,18 @@
+main()
+{
+  char board[501];
+  int s=strlen(gets(board));
+  int i,j,k;
+  for(i=0;board[i];i++)
+  {
+    if(board[i]=='X')
+    {
+      for(j=i;board[j]=='X';j++);
+      k=j-i;
+      if(k&1){puts("-1");return;}
+      for(;i+4<=j;i+=4){board[i]='A';board[i+1]='A';board[i+2]='A';board[i+3]='A';}
+      if(k%4==2){board[i]='B';board[i+1]='B';i+=2;}
+    }
+  }
+  puts(board);
+}
