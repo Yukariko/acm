f77f8906ac4c39c657f22c46bbc2aa9435653b94
Yukariko <rkdals7979@naver.com>
2014-08-19 12:48:34 +0900

    1929

---
+++ problem/1929/test.c
@@ -0,0 +1,22 @@
+main()
+{
+  int m,n,k=0;
+  long long i,j;
+  char che[1000001]={0};
+  int prime[20000];
+  scanf("%d%d",&m,&n);
+  for(i=2;i<=n;i++)
+  {
+    if(che[i]==0)
+    {
+      prime[k++]=i;
+      for(j=i*i;j<=n;j+=i)che[j]=1;
+    }
+  }
+  printf("%d\n",k);
+  while(1);
+  for(i=0;i<k;i++)
+  {
+    if(prime[i]>=m)printf("%d\n",prime[i]);
+  }
+}
