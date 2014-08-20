854b80e5dcc7a74f809fb132fda812cd064cc3cd
Yukariko <rkdals7979@naver.com>
2014-08-20 15:29:25 +0900

    5217

---
+++ problem/5217/test.c
@@ -0,0 +1,17 @@
+main()
+{
+  int t;
+  for(scanf("%d",&t);t--;puts(""))
+  {
+    int n;
+    scanf("%d",&n);
+    printf("Pairs for %d:",n);
+    int i;
+    if(n==2)continue;
+    printf(" 1 %d",n-1);
+    for(i=2;i<n-i;i++)
+    {
+      printf(", %d %d",i,n-i);
+    }
+  }
+}
