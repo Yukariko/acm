a108e9ee9ae550bd768d5326780d11be91cbc8db
Yukariko <rkdals7979@naver.com>
2014-08-24 08:02:22 +0900

    1838

---
+++ problem/1838/test.c
@@ -0,0 +1,28 @@
+cmp(int *a,int *b)
+{
+  return *a>*b?1:*a<*b?-1:0;
+}
+  int a[500001];
+  int b[500001];
+  int warp[500001];
+main()
+{
+
+  int i,n,t,m=1,j;
+  scanf("%d",&n);
+  for(i=0;i<n;i++){scanf("%d",a+i);b[i]=a[i];}
+  qsort(a,n,4,cmp);
+  for(i=0;i<n;i++)
+  {
+    t=(int *)bsearch(&b[i],a,n,4,cmp)-a;
+    if(!warp[t])
+    {
+    	for(j=t;a[j]==b[i];j++);
+        warp[t]=j-1;   
+    }
+    t=warp[t]-1;
+    m=m<i-t?i-t:m;
+  }
+  printf("%d\n",m-1);
+}
+
