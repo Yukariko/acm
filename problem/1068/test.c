6e84b589da9878ce1ed08adb96654f420f0e9b2c
Yukariko <rkdals7979@naver.com>
2014-08-25 21:31:28 +0900

    1068

--- problem/1068/test.c
+++ problem/1068/test.c
@@ -3,7 +3,7 @@ int count,no;
 f(i)
 {
   if(i==no)return;
-  if(a[i][0]==-2&&a[i][1]==-2){count++;return;}
+  if((a[i][0]==-2||a[i][0]==no)&&(a[i][1]==-2||a[i][1]==no)){count++;return;}
   if(a[i][0]!=-2)f(a[i][0]);
   if(a[i][1]!=-2)f(a[i][1]);
 }
@@ -23,7 +23,6 @@ main()
     if(k==-1)t=i;
     else if(a[k][0]==-2)a[k][0]=i;
     else a[k][1]=i;
-    
   }
   scanf("%d",&no);
   f(t);
