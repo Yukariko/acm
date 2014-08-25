ec82a909b77b7342b3c357c83b57f6689e6c61ff
Yukariko <rkdals7979@naver.com>
2014-08-25 15:23:13 +0900

    1874

---
+++ problem/1874/test.c
@@ -0,0 +1,31 @@
+main()
+{
+  int n;
+  scanf("%d",&n);
+  int stack[100001];
+  char print[100001][2]={0};
+  int i,j,k,p;
+  int sIndex=0;
+  int max=0;
+  for(p=i=0;i<n;i++)
+  {
+    scanf("%d",&k);
+    if(max<k)
+    {
+      for(j=max+1;j<=k;j++)
+      {
+        stack[sIndex++]=j;
+        print[p++][0]='+';
+      }
+      sIndex--;
+    }
+    else
+    {
+      if(stack[sIndex-1]!=k){puts("NO");return;}
+      sIndex--;
+    }
+    print[p++][0]='-';
+    max=max<k?k:max;
+  }
+  for(i=0;i<p;i++)puts(print[i]);
+}
