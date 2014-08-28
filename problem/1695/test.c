04698727bf8ad43be7e670e29549b821b7915220
Yukariko <rkdals7979@naver.com>
2014-08-28 12:54:33 +0900

    1695
    not yet

--- problem/1695/test.c
+++ problem/1695/test.c
@@ -1,15 +1,36 @@
 main()
 {
   int n;
-  int stack[5001];
+  int a[5001];
   int i,j,k;
-  int p;
-  for(j=i=0;i<n;i++)
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
   {
-    scanf("%d",&p);
-    for(k=0;k<j;k++)
-    {
-      if(stack[k]==p)
-    }
+    scanf("%d",a+i);
   }
+  int max=0,s;
+  for(i=0;i<n;i++)
+  {
+    s=0;
+    for(j=i,k=i+1;j>=0&&k<n;k++)
+    {
+      if(a[j]==a[k])
+      {
+        s+=2;
+        j--;
+      }
+    }
+    max=max<s?s:max;
+    s=1;
+    for(j=i-1,k=i+1;j>=0&&k<n;k++)
+    {
+      if(a[j]==a[k])
+      {
+        s+=2;
+        j--;
+      }
+    }
+    max=max<s?s:max;
+  }
+  printf("%d",n-max);
 }
