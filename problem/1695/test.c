3b587e9240f9f98ffeba52990019190ffe748a85
Yukariko <rkdals7979@naver.com>
2014-08-28 15:06:23 +0900

    1695

--- problem/1695/test.c
+++ problem/1695/test.c
@@ -1,36 +1,21 @@
+#define MIN(a,b) (a>b?b:a)
+int a[5001];
+short dp[5001][5001];
+f(i,j)
+{
+  if(i>=j)return 0;
+  if(dp[i][j])return dp[i][j];
+  if(a[i]==a[j])return f(i+1,j-1);
+  return dp[i][j]=1+MIN(f(i+1,j),f(i,j-1));
+}
 main()
 {
   int n;
-  int a[5001];
-  int i,j,k;
+  int i;
   scanf("%d",&n);
   for(i=0;i<n;i++)
   {
     scanf("%d",a+i);
   }
-  int max=0,s;
-  for(i=0;i<n;i++)
-  {
-    s=0;
-    for(j=i,k=i+1;j>=0&&k<n;k++)
-    {
-      if(a[j]==a[k])
-      {
-        s+=2;
-        j--;
-      }
-    }
-    max=max<s?s:max;
-    s=1;
-    for(j=i-1,k=i+1;j>=0&&k<n;k++)
-    {
-      if(a[j]==a[k])
-      {
-        s+=2;
-        j--;
-      }
-    }
-    max=max<s?s:max;
-  }
-  printf("%d",n-max);
+  printf("%d",f(0,n-1));
 }
