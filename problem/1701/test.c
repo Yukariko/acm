193e3dc1279c59673a547da02f61e509dd7cefc7
Yukariko <rkdals7979@naver.com>
2014-08-29 16:52:50 +0900

    1701

--- problem/1701/test.c
+++ problem/1701/test.c
@@ -1,21 +1,43 @@
-main()
+make_pi(int *pi,char *p)
 {
-  char a[5001],b[2501]={0};
-  gets(a);
-  int i,j,len;
-  len=strlen(a)/2;
-  
-  for(i=len;i;i--)
+  int i=1,j=0;
+  pi[1]=0;
+  for(;p[i];)
   {
-    strncpy(b,a,i);
-    for(j=i;a[j];j++)
+    if(!j||p[i]==p[j])
     {
-      if(strncmp(a,b,i)==0)
-      {
-        printf("%d",i);return;
-      }
+      j++;i++;pi[i]=j;
     }
-    
+    else j=pi[j];
   }
 }
-
+kmp(int *pi,char *p,char *q,int pi_len)
+{
+  int dp[5002]={};
+  int i,j;
+  for(i=j=1;p[i];)
+  {
+    if(!j||p[i]==q[j]){i++;j++;}
+    else j=pi[j];
+    dp[j]++;
+  }
+  for(j=pi_len+1;j&&dp[j]<2;j--);
+  return j-1;
+}
+main()
+{
+  char a[5002],b[5002];
+  int pi[5002];
+  int i,j;
+  int len1,max=0,res;
+  len1=strlen(gets(a+1));
+  strcpy(b+1,a+1);
+  for(i=0;i<len1;i++)
+  {
+    make_pi(pi,a+i);
+    res=kmp(pi,b,a+i,len1-i);
+    max=max<res?res:max;
+    if(len1-i<=max)break;
+  }
+  printf("%d",max);
+}
