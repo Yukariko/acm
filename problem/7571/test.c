bdbdef2db0fc2330c3c24378299b4a415ba47468
Yukariko <rkdals7979@naver.com>
2014-08-30 01:02:28 +0900

    7571

--- problem/7571/test.c
+++ problem/7571/test.c
@@ -1,14 +1,12 @@
-typedef struct point{int x,y;}point;
-int x[100001];
-int y[100001];
-int dp[100001];
 cmp(int *a,int *b){return *a>*b?1:*a<*b?-1:0;}
 main()
 {
   int n,m;
-  scanf("%d%d",&n,&m);
+  int x[100001];
+  int y[100001];
   int i,j;
   int px,py;
+  scanf("%d%d",&n,&m);
   for(i=0;i<m;i++)
   {
     scanf("%d%d",&x[i],&y[i]);
@@ -19,11 +17,9 @@ main()
   py=y[m/2];
   int t;
   int s;
-  s=0;
-  for(i=0;i<m;i++)
+  for(s=i=0;i<m;i++)
   {
-    t=abs(px-x[i])+abs(py-y[i]);
-    s+=t;
+    s+=abs(px-x[i])+abs(py-y[i]);
   }
   printf("%d",s);
 }
