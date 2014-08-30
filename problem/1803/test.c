9f7751b89e46111b728a2de08cd1b3147cbe2541
Yukariko <rkdals7979@naver.com>
2014-08-30 22:31:04 +0900

    1803

--- problem/1803/test.c
+++ problem/1803/test.c
@@ -14,9 +14,6 @@ main()
     for(i=1;i<=m;i++)
     {
       if(!selectA[i]&&markA[i]==0)
-      //얘를 노리는 사람이 없음.
-      //즉, 얘는 무조건 활. 또한 얘의 표적은 무조건 방패. 
-      //방패를 든 학생한태 표적이 된 학생은 노려진 횟수 감소. 
       {
         t=B[A[i]];
         selectA[i]='1';
@@ -29,7 +26,6 @@ main()
     for(i=1;i<=n;i++)
     {
       if(!selectB[i]&&markB[i]==0)
-      //위와 같음 
       {
         t=A[B[i]];
         selectB[i]='1';
