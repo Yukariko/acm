3a2fd2767f2e1ec54702d6fd26450054f13fde97
Yukariko <rkdals7979@naver.com>
2014-09-01 19:20:34 +0900

    1915

--- problem/1915/test.c
+++ problem/1915/test.c
@@ -1,22 +1,8 @@
+#define MAX(a,b) (a>b?a:b)
+#define MIN(a,b) (a>b?b:a)
 char map[1001][1001];
+int dp[1001][1001];
 int max;
-f(i,j)
-{
-  int p,q,r;
-  for(r=max;;r++)
-  {
-    for(p=0;p<r;p++)
-    {
-      for(q=0;q<r;q++)
-      {
-        if(map[i+p][j+q]!='1')
-        {
-          return r-1;
-        }
-      }
-    }
-  }
-}
 main()
 {
   int n,m;
@@ -24,14 +10,16 @@ main()
   scanf("%d%d ",&n,&m);
   int i,j,s;
   for(i=0;i<n;i++)gets(map[i]);
-  for(i=0;i<n;i++)
+  for(i=0;i<m;i++)if(map[0][i]=='1'){dp[0][i]=1;max=1;}
+  for(i=0;i<n;i++)if(map[i][0]=='1'){dp[i][0]=1;max=1;}
+  for(i=1;i<n;i++)
   {
-    for(j=0;j<m;j++)
+    for(j=1;j<m;j++)
     {
       if(map[i][j]=='1')
       {
-        s=f(i,j);
-        max=max<s?s:max;
+        dp[i][j]=1+MIN(MIN(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+        max=MAX(max,dp[i][j]);
       }
     }
   }
