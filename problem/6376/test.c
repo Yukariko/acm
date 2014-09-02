821711c22ae894e1f2486bed4a196b5c3b57abf4
Yukariko <rkdals7979@naver.com>
2014-09-02 21:41:18 +0900

    6376

---
+++ problem/6376/test.c
@@ -0,0 +1,15 @@
+main()
+{
+  puts("n e");
+  puts("- -----------");
+  puts("0 1");
+  long long i,j;
+  double s=1;
+  for(i=j=1;i<=9;j*=++i)
+  {
+    s+=(double)1/j;
+    if(i==1)puts("1 2");
+    if(i==2)puts("2 2.5");
+    if(i>2)printf("%d %.9lf\n",i,s);
+  }
+}
