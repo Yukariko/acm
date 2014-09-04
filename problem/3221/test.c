6471f2546cdb465cb90f9f3733c05ebdc4ce5dd0
Yukariko <rkdals7979@naver.com>
2014-09-04 18:53:38 +0900

    3221

--- problem/3221/test.c
+++ problem/3221/test.c
@@ -2,50 +2,50 @@ typedef struct ant{int pos;char move;}ant;
 cmp(ant *a,ant *b){return a->pos>b->pos?1:a->pos<b->pos?-1:0;}
 main()
 {
-    int l,t;
-    int n;
-    scanf("%d%d",&l,&t);
-    scanf("%d",&n);
-    ant a[70001];
-    int i;
-    for(i=0;i<n;i++)
+  int l,t;
+  int n;
+  scanf("%d%d",&l,&t);
+  scanf("%d",&n);
+  ant a[70001];
+  int i;
+  for(i=0;i<n;i++)
+  {
+    scanf("%d %c",&a[i].pos,&a[i].move);
+  }
+  int p,q;
+  for(i=0;i<n;i++)
+  {
+    int k=t;
+    if(a[i].move=='L')
     {
-        scanf("%d %c",&a[i].pos,&a[i].move);
+      if(k>=a[i].pos)
+      {
+        k-=a[i].pos;
+        p=k/l;
+        q=k%l;
+        a[i].pos=p%2?l-q:q;
+      }
+      else
+      {
+        a[i].pos-=k;
+      }
     }
-    int p,q;
-    for(i=0;i<n;i++)
+    else
     {
-        int k=t;
-        if(a[i].move=='L')
-        {
-            if(k>=a[i].pos)
-            {
-                k-=a[i].pos;
-                p=k/l;
-                q=k%l;
-                a[i].pos=p%2?l-q:q;
-            }
-            else
-            {
-                a[i].pos-=k;
-            }
-        }
-        else
-        {
-            if(k>=l-a[i].pos)
-            {
-                k-=l-a[i].pos;
-                p=k/l;
-                q=k%l;
-                a[i].pos=p%2?q:l-q;
-            }
-            else
-            {
-                a[i].pos+=k;
-            }
-        }
+      if(k>=l-a[i].pos)
+      {
+        k-=l-a[i].pos;
+        p=k/l;
+        q=k%l;
+        a[i].pos=p%2?q:l-q;
+      }
+      else
+      {
+        a[i].pos+=k;
+      }
     }
-    qsort(a,n,sizeof(ant),cmp);
-    for(i=0;i<n;i++)printf("%d ",a[i].pos);
+  }
+  qsort(a,n,sizeof(ant),cmp);
+  for(i=0;i<n;i++)printf("%d ",a[i].pos);
 }
 
