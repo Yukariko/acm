e0521a970d8ea8b45458d970c5c54a93b236ba7c
Yukariko <rkdals7979@naver.com>
2014-09-06 13:44:42 +0900

    Accept

--- problem/1990/test.c
+++ problem/1990/test.c
@@ -1,36 +1,50 @@
-#define M 100000000
+int prime[1350];
 isPrime(p)
 {
   int i;
-  for(i=2;i*i<=p;i++)
+  for(i=0;prime[i]*prime[i]<=p;i++)
   {
-    if(p%i==0)return 0;
+    if(p%prime[i]==0)return 0;
   }
   return 1;
 }
-isPalin(char *b,int len)
+char a[10];
+int palin[790];
+int k;
+f(i,n)
 {
-  int i=len/2;
-  if(len%2==0)
+  int p,q;
+  if(i>n/2){q=atoi(a);if(q<=100030001&&isPrime(q)){palin[k++]=q;}return;}
+  if(i){a[i]='0';a[n-i]='0';f(i+1,n);}
+  for(p=1;p<=9;p++)
   {
-    int p,q;
-    p=len/2-1;q=len/2;
-    for(;p>=0&&q<len;p--,q++)if(b[p]!=b[q])return 0;
+    a[i]=p+'0';
+    a[n-i]=p+'0';
+    f(i+1,n);
   }
-  else
-  {
-    int p,q;
-    p=len/2;q=p;    
-    for(;p>=0&&q<len;p--,q++)if(b[p]!=b[q])return 0;
-  }
-  return 1;
-}
-f(n)
-{
-  
 }
 main()
 {
+  int i,j,p=0;
+  char che[11001]={};
+  for(i=2;i<=11000;i++)
+  {
+    if(!che[i])
+    {
+      prime[p++]=i;
+      for(j=i*i;j<=11000;j+=i)che[j]=1;
+    }
+  }
+  for(i=0;i<=8;i++)
+  {
+    f(0,i);
+  }
   int a,b;
   scanf("%d%d",&a,&b);
+  for(i=0;palin[i]<a;i++);
+  for(;palin[i]<=b;i++)
+  {
+    printf("%d\n",palin[i]);
+  }
+  puts("-1");
 }
