965fcccc0989602deb9725ea8be989ebf32cf5d2
Yukariko <rkdals7979@naver.com>
2014-09-06 16:55:46 +0900

    Accept

--- problem/1168/test.c
+++ problem/1168/test.c
@@ -1,22 +1,22 @@
 main()
 {
-    int n,M;
-    int a[100001]={0};
-    scanf("%d%d",&n,&M);
-    int i,j,k,t;
-    for(i=0;i<n;i++)a[i]=i+1;
-    printf("<");
-    for(i=-1,j=n;j;j--)
+  int n,M;
+  int a[100001];
+  scanf("%d%d",&n,&M);
+  int i,j,k,t;
+  for(i=0;i<n;i++)a[i]=i+1;
+  printf("<");
+  for(i=-1,j=n;j;j--)
+  {
+    i=(i+M)%j;
+    if(j!=1)printf("%d, ",a[i]);
+    else printf("%d",a[i]);
+    for(k=i;k<j;k++)
     {
-      i=(i+M)%j;
-      if(j!=1)printf("%d, ",a[i]);
-      else printf("%d",a[i]);
-      for(k=i;k<j;k++)
-      {
-        a[k]=a[k+1];
-      }
-      i--;
+      a[k]=a[k+1];
     }
-    printf(">");
+    i--;
+  }
+  printf(">");
 }
 
