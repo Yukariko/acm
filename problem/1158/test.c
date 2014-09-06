4ffb62f344b8779ebd0275a0c083eccaf26a9c61
Yukariko <rkdals7979@naver.com>
2014-09-06 16:52:19 +0900

    Accept

--- problem/1158/test.c
+++ problem/1158/test.c
@@ -1,48 +1,22 @@
-#define MAX_QUEUE 1
-typedef int DATA;
-typedef struct queue{DATA data;struct queue *next;}queue;
-queue *head[MAX_QUEUE],*tail[MAX_QUEUE];
-void push(int index,DATA val)
-{
-  tail[index]->data=val;
-  tail[index]->next=(queue *)malloc(sizeof(queue));
-  tail[index]=tail[index]->next;
-  tail[index]->next=0;
-}
-void init(int index)
-{
-  head[index]=(queue *)malloc(sizeof(queue));
-  head[index]->next=0;
-  tail[index]=head[index];
-}
 main()
 {
-  int n,M;
-  queue *q,*qh;
-  init(0);
-  scanf("%d%d",&n,&M);
-  int i,j,k;
-  for(i=1;i<=n;i++)push(0,i);
-  for(q=head[0];q->next->next;q=q->next);
-  qh=q->next;
-  q->next=head[0];
-  q=head[0];
-  printf("<");
-  for(j=n;j--;)
-  {
-    for(k=M;--k;)
+    int n,M;
+    int a[5001]={0};
+    scanf("%d%d",&n,&M);
+    int i,j,k,t;
+    for(i=0;i<n;i++)a[i]=i+1;
+    printf("<");
+    for(i=-1,j=n;j;j--)
     {
-      qh=q;
-      q=q->next;
+      i=(i+M)%j;
+      if(j!=1)printf("%d, ",a[i]);
+      else printf("%d",a[i]);
+      for(k=i;k<j;k++)
+      {
+        a[k]=a[k+1];
+      }
+      i--;
     }
-    i=q->data;
-    qh->next=q->next;
-    q=q->next;
-
-    if(j!=0)printf("%d, ",i);
-    else printf("%d",i);
-
-  }
-  printf(">");
+    printf(">");
 }
 
