559dcd9bdabe33c5e3c51f1063aff3c7c0569993
Yukariko <rkdals7979@naver.com>
2014-09-07 15:28:53 +0900

    Wrong Answer

--- problem/7573/test.c
+++ problem/7573/test.c
@@ -1,6 +1,6 @@
 #define MAX(a,b) (a>b?a:b)
 #define MIN(a,b) (a<b?a:b)
-char map[10022][10022]={0};
+char map[10022][10022];
 typedef struct FISH{int x,y;}FISH;
 FISH fish[101];
 main()
@@ -18,13 +18,13 @@ main()
   }
   int p,q,r,s;
   int j,k,max=0;
-  //puts("---");
   for(i=0;i<M;i++)
   {
     for(p=1;2*p+2<=L;p++)
     {
       q=(L-2*p)/2;
-    //  printf("%d %d\n",p,q);
+      printf("%d %d\n",p,q);
+      if(q<=0||p>N||q>N)continue;
       if(fish[i].x+p<=N&&fish[i].y+q<=N)
       {
         s=0;
@@ -36,7 +36,40 @@ main()
         }
         max=max<s?s:max;
       }
-      else if(fish[i].x+p<=N&&N-q>=1)
+      if(fish[i].x+p<=N&&fish[i].y-q>=1)
+      {
+        s=0;
+        j=fish[i].x+p;
+        k=fish[i].y-q;
+        for(x=fish[i].x;x<=j;x++)
+        {
+          for(y=fish[i].y;y>=k;y--)if(map[y][x])s++;
+        }
+        max=max<s?s:max;
+      }
+      if(fish[i].x-p>=1&&fish[i].y+q<=N)
+      {
+        s=0;
+        j=fish[i].x-p;
+        k=fish[i].y+q;
+        for(x=fish[i].x;x>=j;x--)
+        {
+          for(y=fish[i].y;y<=k;y++)if(map[y][x])s++;
+        }
+        max=max<s?s:max;
+      }
+      if(fish[i].x-p>=1&&fish[i].y-q>=1)
+      {
+        s=0;
+        j=fish[i].x-p;
+        k=fish[i].y-q;
+        for(x=fish[i].x;x>=j;x--)
+        {
+          for(y=fish[i].y;y>=k;y--)if(map[y][x])s++;
+        }
+        max=max<s?s:max;
+      }
+      if(fish[i].x+p<=N&&fish[i].y+q>N)
       {
         s=0;
         j=fish[i].x+p;
@@ -47,7 +80,40 @@ main()
         }
         max=max<s?s:max;
       }
-      else if(fish[i].y+q<=N&&N-p>=1)
+      if(fish[i].x+p<=N&&fish[i].y-q<1)
+      {
+        s=0;
+        j=fish[i].x+p;
+        k=1;
+        for(x=fish[i].x;x<=j;x++)
+        {
+          for(y=q;y>=k;y--)if(map[y][x])s++;
+        }
+        max=max<s?s:max;
+      }
+      if(fish[i].x-p>=1&&fish[i].y+q>N)
+      {
+        s=0;
+        j=fish[i].x-p;
+        k=N;
+        for(x=fish[i].x;x>=j;x--)
+        {
+          for(y=N-q;y<=k;y++)if(map[y][x])s++;
+        }
+        max=max<s?s:max;
+      }
+      if(fish[i].x-p>=1&&fish[i].y-q<1)
+      {
+        s=0;
+        j=fish[i].x-p;
+        k=1;
+        for(x=fish[i].x;x>=j;x--)
+        {
+          for(y=q;y>=k;y--)if(map[y][x])s++;
+        }
+        max=max<s?s:max;
+      }
+      if(fish[i].x+p>N&&fish[i].y+q<=N)
       {
         s=0;
         j=N;
@@ -58,16 +124,82 @@ main()
         }
         max=max<s?s:max;        
       }
-      else if(N-p>=1&&N-q>=1)
+      if(fish[i].x-p<1&&fish[i].y+q<=N)
+      {
+        s=0;
+        j=1;
+        k=fish[i].y+q;
+        for(x=p;x>=j;x--)
+        {
+          for(y=fish[i].y;y<=k;y++)if(map[y][x])s++;
+        }
+        max=max<s?s:max;        
+      }
+      if(fish[i].x+p>N&&fish[i].y-q>=1)
+      {
+        s=0;
+        j=N;
+        k=fish[i].y-q;
+        for(x=N-p;x<=j;x++)
+        {
+          for(y=fish[i].y;y>=k;y--)if(map[y][x])s++;
+        }
+        max=max<s?s:max;        
+      }
+      if(fish[i].x-p<1&&fish[i].y-q>=1)
+      {
+        s=0;
+        j=1;
+        k=fish[i].y-q;
+        for(x=p;x>=j;x--)
+        {
+          for(y=fish[i].y;y>=k;y--)if(map[y][x])s++;
+        }
+        max=max<s?s:max;        
+      }
+      if(fish[i].x-p<1&&fish[i].y-q<1)
+      {
+        s=0;
+        j=1;
+        k=1;
+        for(x=p;x>=j;x--)
+        {
+          for(y=q;y>=k;y--)if(map[y][x])s++;
+        }
+        max=max<s?s:max;        
+      }
+      if(fish[i].x-p<1&&fish[i].y+q>N)
+      {
+        s=0;
+        j=1;
+        k=N;
+        for(x=p;x>=j;x--)
+        {
+          for(y=N-q;y<=k;y++)if(map[y][x])s++;
+        }
+        max=max<s?s:max;        
+      }
+      if(fish[i].x+p>N&&fish[i].y-q<1)
+      {
+        s=0;
+        j=N;
+        k=1;
+        for(x=N-p;x<=j;x++)
+        {
+          for(y=q;y>=k;y--)if(map[y][x])s++;
+        }
+        max=max<s?s:max;        
+      }
+      if(fish[i].x+p>N&&fish[i].y+q>N)
       {
         s=0;
         j=N;
         k=N;
-        for(x=N-p;x<=j;x++)
+        for(x=N-p;x>=j;x--)
         {
-          for(y=N-q;y<=k;y++)if(map[y][x])s++;
+          for(y=N-q;y>=k;y--)if(map[y][x])s++;
         }
-        max=max<s?s:max; 
+        max=max<s?s:max;        
       }
     }
   }
