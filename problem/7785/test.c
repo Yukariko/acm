d2d360db878db1669c843e8c5f899f18ea99b76a
Yukariko <rkdals7979@naver.com>
2014-09-10 21:59:06 +0900

    Accept

---
+++ problem/7785/test.c
@@ -0,0 +1,18 @@
+cmp(char *a,char *b){return strcmp(b,a);}
+char a[100001][12];
+main()
+{
+  int n;
+  
+  scanf("%d ",&n);
+  int i,j,k;
+  for(i=0;i<n;i++)gets(a[i]);
+  qsort(a,n,12,cmp);
+  for(i=0;i<n;i++)
+  {
+    for(j=0;a[i][j]!=' ';j++);
+    for(k=0;a[i+k][j+1]=='l';k++);
+    if(k)i+=k*2-1;
+    else {a[i][j]=0;puts(a[i]);}
+  }
+}
