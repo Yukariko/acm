2835e036ed985e29ba9e01aa6831fd8ad3a91a03
Yukariko <rkdals7979@naver.com>
2014-09-15 01:28:29 +0900

    Accept

--- problem/10158/test.c
+++ problem/10158/test.c
@@ -6,25 +6,17 @@ main()
   int p,q,t;
   scanf("%d%d%d",&p,&q,&t);
   int x=1,y=1,k;
-  int sx,sy,st,c;
-  for(c=0;t;)
+  for(;t;)
   {
     k=MIN((x==1?w-p:p),(y==1?h-q:q));
     k=MIN(k,t);
     t-=k;
     p+=k*x;
     q+=k*y;
-    if(x==1&&p==w)x=-1;
-    else if(x==-1&&p==0)x=1;
-    if(y==1&&q==h)y=-1;
-    else if(y==-1&&q==0)y=1;
-    if(c==0){sx=p;sy=q;st=t;c=1;}
-    else if(sx==p&&sy==q&&c==1)
-    {
-      t%=t-st;
-      c=2;
-    }
-    printf("%d %d %d\n",p,q,t);
+    if(p==w)x=-1;
+    else if(p==0)x=1;
+    if(q==h)y=-1;
+    else if(q==0)y=1;
   }
   printf("%d %d",p,q);
 }
