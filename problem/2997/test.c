92d60d399a409d1f27a0156a34fc422c42bd9465
Yukariko <rkdals7979@naver.com>
2014-09-16 14:23:03 +0900

    Accept

--- problem/2997/test.c
+++ problem/2997/test.c
@@ -1,11 +1,10 @@
-f(a,b){return b?f(b,a%b):a;}
 cmp(int *a,int *b){return *a-*b;}
 main()
 {
   int a[4];
+  int x,y;
   scanf("%d%d%d",a,a+1,a+2);
   qsort(a,3,4,cmp);
-  if(a[1]-a[0]==a[2]-a[1])return printf("%d",a[2]+a[2]-a[1]),0;
-  if(a[1]-a[0]>a[2]-a[1])printf("%d",a[0]+f(a[1]-a[0],a[2]-a[1]));
-  if(a[1]-a[0]<a[2]-a[1])printf("%d",a[1]+f(a[1]-a[0],a[2]-a[1]));
+  x=a[1]-a[0];y=a[2]-a[1];
+  printf("%d",x==y?a[2]+x:x>y?a[0]+y:a[1]+x);
 }
