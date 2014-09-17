db84dc3806b94f34e9a4d57c239f3d91fa9f1af7
Yukariko <rkdals7979@naver.com>
2014-09-17 20:33:26 +0900

    Wrong Answer

--- problem/9997/test.c
+++ problem/9997/test.c
@@ -1,28 +1,36 @@
+int n;
+char word[26][101];
+char alpha[26];
+int visit[26];
+int count;
+f(p,c)
+{
+  if(c==0){count++;}
+  int i,j,t;
+  for(i=p;i<n;i++)
+  {
+    t=0;
+    for(j=0;word[i][j];j++)
+    {
+      t+=alpha[word[i][j]]++==0;
+    }
+    visit[i]=1;
+    f(i+1,c-t);
+    visit[i]=0;
+    for(j=0;word[i][j];j++)
+    {
+      alpha[word[i][j]]--;
+    }
+  }
+}
 main()
 {
-  int n;
-  char word[26][101];
-  char alpha[26][26]={};
   scanf("%d ",&n);
   int i,j,k,s;
   for(i=0;i<n;i++)
   {
     gets(word[i]);
-    for(j=0;word[i][j];j++)alpha[i][word[i][j]-'a']++;
   }
-  for(s=i=0;i<n;i++)
-  {
-    for(j=i+1;j<n;j++)
-    {
-      for(k=0;k<26;k++)
-      {
-        if(!alpha[i][k]&&!alpha[j][k])
-        {
-          break;
-        }
-      }
-      if(k==26)s++;
-    }
-  }
-  printf("%d",s);
+  f(0,26);
+  printf("%d",count);
 }
