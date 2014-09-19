06a5ab58abd5051fe442b0c8c6af66db477fda6c
Yukariko <rkdals7979@naver.com>
2014-09-19 18:19:46 +0900

    Wrong Answer

---
+++ problem/3991/test.c
@@ -0,0 +1,32 @@
+main()
+{
+  int h,w,c;
+  scanf("%d%d%d",&h,&w,&c);
+  int a[9];
+  int i,j,k;
+  for(i=1;i<=c;i++)scanf("%d",a+i);
+  char map[11][11]={};
+  k=1;
+  for(i=0;i<w;i++)
+  {
+    for(j=h-1;j>=0;j--)
+    {
+      if(a[k])
+      {
+        map[j][i]=k+'0';
+        a[k]--;
+      }
+      else
+      {
+        k++;
+        if(k>c)break;
+        j++;
+      }
+    }
+    if(j>=0)break;
+  }
+  for(i=0;i<h;i++)
+  {
+    puts(map[i]);
+  }
+}
