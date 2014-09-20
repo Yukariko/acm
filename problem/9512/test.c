01578a421630d684e3a2638f175bd3c8e6e674d3
Yukariko <rkdals7979@naver.com>
2014-09-21 02:50:25 +0900

    Acceot

---
+++ problem/9512/test.c
@@ -0,0 +1,68 @@
+char word[101][130][258];
+char frontWord[101][258];
+int n;
+int count[101];
+find(char *ck)
+{
+  int i,j;
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<count[i];j++)
+    {
+      if(strcmp(ck,word[i][j])==0)return i;
+    }
+  }
+  return -1;
+}
+main()
+{
+
+  scanf("%d ",&n);
+  int i,j,k;
+  int p;
+  char a[258];
+  for(i=0;i<n;i++)
+  {
+    scanf("%s",frontWord[i]);
+    gets(a);
+    for(k=j=0;a[j];j++)
+    {
+      if((a[j]>='a'&&a[j]<='z')||a[j]=='\''||a[j]=='-'||(a[j]>='A'&&a[j]<='Z'))
+      {
+        word[i][count[i]][k]=toupper(a[j]);
+        k++;
+      }
+      else if(k){count[i]++;k=0;}
+    }
+    if(k)count[i]++;
+  }
+  char aword[258];
+  gets(a);
+  for(;gets(a);)
+  {
+    for(k=i=0;a[i];i++)
+    {
+      if((a[i]>='a'&&a[i]<='z')||a[i]=='\''||a[i]=='-'||(a[i]>='A'&&a[i]<='Z'))
+      {
+        aword[k++]=toupper(a[i]);
+      }
+      else if(k)
+      {
+        aword[k]=0;
+        k=0;
+        j=find(aword);
+        if(j!=-1)
+        {
+          puts(frontWord[j]);
+          break;
+        }
+      }
+    }
+    if(k)
+    {
+      aword[k]=0;
+      j=find(aword);
+      if(j!=-1)puts(frontWord[j]);
+    }
+  }
+}
