39a93f6fb8cc4e1f511486e0df28356e82f842bb
Yukariko <rkdals7979@naver.com>
2014-09-23 05:19:35 +0900

    Accept

--- problem/2805/test.c
+++ problem/2805/test.c
@@ -1,10 +1,41 @@
+int a[1000001];
 main()
 {
   int n,m;
-  int a[1000001];
   scanf("%d%d",&n,&m);
   int i;
-  for(i=0;i<n;i++)
+  int max,min;
+  scanf("%d",a);
+  max=min=a[0];
+  for(i=1;i<n;i++)
   {
+    scanf("%d",a+i);
+    if(max<a[i])
+    {
+      max=a[i];
+    }
+    else if(min>a[i])
+    {
+      min=a[i];
+    }
   }
+  int mid;
+  int res=m,sel;
+  for(;min<=max;)
+  {
+    mid=(max+min)/2;
+    long long s=0;
+    for(i=0;i<n;i++)if(mid<a[i])s+=a[i]-mid;
+    if(s>=m)
+    {
+      min=mid+1;
+      if(res>s-m)
+      {
+        res=s-m;
+        sel=mid;
+      }
+    }
+    else if(s<m)max=mid-1;
+  }
+  printf("%d",sel);
 }
