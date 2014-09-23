7b9ec8a8425933684b985585146d5f8d69ab9349
Yukariko <rkdals7979@naver.com>
2014-09-24 01:25:37 +0900

    Accept

--- problem/1188/test.c
+++ problem/1188/test.c
@@ -1,6 +1,12 @@
+f(a,b){return b?f(b,a%b):a;}
 main()
 {
-  int n,m;
+  int n,m,k;
   scanf("%d%d",&n,&m);
-  printf("%d",m/n-1+n);
+  k=f(n,m);
+  int a,b;
+  a=n/k;
+  b=m/k;
+  printf("%d",n*(b-1)/a);
+  
 }
