8851ae65bd5d941f5be3bb640cfbd899a62a038b
Yukariko <rkdals7979@naver.com>
2014-09-23 11:06:18 +0900

    Accept

--- problem/1132/test.c
+++ problem/1132/test.c
@@ -1,43 +1,63 @@
-typedef struct SCORE{double s;int no;}SCORE;
-cmp(SCORE *a,SCORE *b){return b->s>a->s?1:a->s>b->s?-1:0;}
-main()
+char map[51][13];
+int n;
+long long point[10];
+int sel[10];
+int front[10];
+long long f()
 {
-  int n;
-  int count[13][11]={0};
-  char a[51][13];
-  SCORE score[11]={0};
-  int encode[11];
-  int i,j,k,l=0;
-  scanf("%d ",&n);
-  for(j=0;j<n;j++)
-  {
-    k=strlen(gets(a[j]));
-    l=12;
-    for(i=k-1;i>=0;i--,l--)count[l][a[j][i]-'A']++;
-  }
-  for(i=0;i<10;i++)score[i].no=i;
-  double s=1;
-  for(k=12;k>=0;k--)
-  {
-    for(i=0;i<10;i++)
-    {
-      score[i].s+=count[k][i]*s;
-    }
-    s*=10;
-  }
-  qsort(score,10,sizeof(SCORE),cmp);
-  j=9;
-  for(i=0;i<10;i++)
-  {
-    encode[score[i].no]='0'+j--;
-  }
-  long long p=0;
+  int i,j;
+  long long t,s=0;
   for(i=0;i<n;i++)
   {
-    for(j=0;a[i][j];j++)a[i][j]=encode[a[i][j]-'A'];
-    a[i][j]=0;
-    puts(a[i]);
-    p+=atol(a[i]);
+    for(t=j=0;map[i][j];j++)
+    {
+      t=t*10+sel[map[i][j]-'A'];
+    }
+    s+=t;
   }
-  printf("%lld",p);
+  return s;
+}
+main()
+{
+  scanf("%d ",&n);
+  int i,j;
+  for(i=0;i<n;i++)
+  {
+    long long k=1;
+    for(j=strlen(gets(map[i]))-1;j>=0;j--)
+    {
+      point[map[i][j]-'A']+=k;
+      k*=10;
+    }
+    front[map[i][0]-'A']++;
+  }
+  for(i=0;i<10;i++)if(point[i]==0)break;
+  if(i==10)
+  {
+    int min=-1;
+    for(i=0;i<10;i++)
+    {
+      if(front[i]==0)
+      {
+        if(min==-1)min=i;
+        else min=point[min]>point[i]?i:min;
+      }
+    }
+    sel[min]=0;
+    point[min]=-1;
+  }
+  for(i=9;i>=1;i--)
+  {
+    long long max=0;
+    for(j=0;j<10;j++)
+    {
+      max=point[max]<point[j]?j:max;
+    }
+    if(point[max]!=-1)
+    {
+      sel[max]=i;
+      point[max]=-1;
+    }
+  }
+  printf("%lld",f());
 }
