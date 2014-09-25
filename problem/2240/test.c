f06d784cc7d658aa6728312367763ed15a08259a
Yukariko <rkdals7979@naver.com>
2014-09-25 18:41:00 +0900

    Accept
    more efficient

--- problem/2240/test.c
+++ problem/2240/test.c
@@ -1,38 +1,36 @@
 #define MAX(a,b) (a>b?a:b)
-int a[1001][2];
+int a[1001];
 int k;
-int dp[1001][2][31];
-f(i,s,w)
+int dp[1001][31];
+f(i,w)
 {
   if(i>k)return 0;
-  if(dp[i][s][w])return dp[i][s][w];
-  if(w==0)return dp[i][s][w]=a[i][1]+f(i+2,s,w);
-  return dp[i][s][w]=a[i][1]+MAX(f(i+1,a[i+1][0],w-1),f(i+2,s,w));
+  if(dp[i][w])return dp[i][w];
+  if(w==0)return dp[i][w]=a[i]+f(i+2,w);
+  return dp[i][w]=a[i]+MAX(f(i+1,w-1),f(i+2,w));
 }
 main()
 {
   int t,w;
-
   scanf("%d%d",&t,&w);
-  int i,j,s;
+  int i,j,s,sel;
   scanf("%d",&j);
+  sel=j;
   s=j;
   k=0;
-  a[0][0]=j;
-  a[0][1]=1;
+  a[0]=1;
   for(i=1;i<t;i++)
   {
     scanf("%d",&j);
     if(s==j)
     {
-      a[k][1]++;
+      a[k]++;
     }
     else
     {
       s=j;
-      a[++k][0]=j;
-      a[k][1]=1;
+      a[++k]=1;
     }
   }
-  printf("%d",MAX(a[0][0]==1?f(0,1,w):f(1,1,w),f(0,2,w-1)));
+  printf("%d",MAX(sel==1?f(0,w):f(1,w),f(0,w-1)));
 }
