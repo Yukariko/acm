c5142ee5a6877184a634b971d10a3e533b1f7c18
Yukariko <rkdals7979@naver.com>
2014-09-29 20:41:30 +0900

    Accept

--- problem/9037/test.c
+++ problem/9037/test.c
@@ -3,18 +3,12 @@ main()
   int t;
   for(scanf("%d",&t);t--;)
   {
-    int n;
+    int n,i,s,next;
+    int a[11],b[11]={};
     scanf("%d",&n);
-    int i,j;
-    int a[11];
-    int b[11]={};
-    for(i=0;i<n;i++)
-    {
-      scanf("%d",a+i);
-    }
-    int s=0,next;
+    for(i=0;i<n;i++)scanf("%d",a+i);
     for(i=0;i<n;i++)a[i]+=a[i]%2;
-    for(;;s++)
+    for(s=0;;s++)
     {
       for(i=1;i<n;i++)if(a[i-1]!=a[i])break;
       if(i==n)break;
