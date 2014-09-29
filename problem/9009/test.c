d4f6fa23958b24fa21f0433f8a1ec764764a63a2
Yukariko <rkdals7979@naver.com>
2014-09-29 19:34:47 +0900

    Accept

--- problem/9009/test.c
+++ problem/9009/test.c
@@ -1,29 +1,36 @@
+cmp(int *a,int *b){return *a>*b?1:*a<*b?-1:0;}
+int fib[43];
+int res[43];
+f(p,s,c)
+{
+  int r=0;
+  if(s==0)return c;
+  if(p<0)return -1;
+  if(fib[p]<=s)
+  {
+    res[c]=fib[p];
+    r=f(p-1,s-fib[p],c+1);
+  }
+  if(r==-1||fib[p]>s)return f(p-1,s,c);
+  return r;
+}
 main()
 {
-  int fib[43];
   int i=1,j=0,k=0;
-  for(;i<=1000000000;i++)
+  for(;i<=1000000000;)
   {
     fib[k++]=i;
     i+=j;
     j=i-j;
   }
   int t;
-  int a[43];
   for(scanf("%d",&t);t--;)
   {
     int n;
     scanf("%d",&n);
-    j=0;
-    for(i=k-1;i>=0;i--)
-    {
-      if(fib[i]>=n)
-      {
-        n-=fib[i];
-        a[j++]=fib[i];
-      }
-    }
-    for(i=0;i<j;i++)printf("%d ",a[i]);
+    j=f(k-1,n,0);
+    qsort(res,j,4,cmp);
+    for(i=0;i<j;i++)printf("%d ",res[i]);
     puts("");
   }
 }
