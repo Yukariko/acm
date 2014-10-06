0d39f62a82d6d90ba5c6c004abce6531ae7ed8d1
Yukariko <rkdals7979@naver.com>
2014-10-06 14:45:58 +0900

    Accept

--- problem/3933/test.c
+++ problem/3933/test.c
@@ -1,41 +1,24 @@
 main()
 {
   int sq[182];
-  char che[32769]={};
-  int count[32769]={};
-  int tcount[32769];
+  int dp[32769][5]={};
   int i,j,k=0;
-  for(i=1;i*i<=32768;i++)sq[k++]=i*i;
-  for(i=0;sq[i]<=32768;i++)
+  for(i=1;i*i<=32768;i++)sq[i-1]=i*i;
+  dp[0][0]=1;
+  for(j=0;j<181;j++)
   {
-    che[sq[i]]++;
-    count[sq[i]]++;
-  }
-  for(j=1;j<4;j++)
-  {
-    memset(tcount,0,sizeof(tcount));
-    for(k=0;k<181;k++)
+    for(k=sq[j];k<=32768;k++)
     {
-      for(i=1;i<32768;i++)
+      for(i=1;i<=4;i++)
       {
-        if(che[i]==j)
-        {
-          tcount[i+sq[k]]=1;
-        }
-      }
-    }
-    for(i=1;i<=32768;i++)
-    {
-      if(tcount[i])
-      {
-        count[i]+=tcount[i];
-        che[i]=j+1;
+        dp[k][i]+=dp[k-sq[j]][i-1];
       }
     }
   }
+  
   int n;
   for(;scanf("%d",&n),n;)
   {
-    printf("%d\n",count[n]);
+    printf("%d\n",dp[n][1]+dp[n][2]+dp[n][3]+dp[n][4]);
   }
 }
