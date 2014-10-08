87848a09edef8239e29fce922b97e25094a7eefb
Yukariko <rkdals7979@naver.com>
2014-10-08 13:10:42 +0900

    Wrong Answer

--- problem/2823/test.c
+++ problem/2823/test.c
@@ -1,57 +1,52 @@
-#define UP 1
-#define DOWN 2
-#define LEFT 3
-#define RIGHT 4
-min(a,b){return a>b?b:a;}
-char map[13][13];
-int mask[13][13];
-int R,C,x,y,m;
-int now,past,sel;
-main(i,j,k)
+int r,c;
+int sx,sy;
+int numLoad,count,end;
+char map[11][11];
+char visit[5][11][11];
+isOK(x,y,t)
 {
-  scanf("%d%d ",&R,&C);
-  for(i=1;i<=R;i++)gets(&map[i][1]);
-  for(i=1;i<=R;i++)for(j=1;j<=C;j++){m+=map[i][j]=='.';x=j;y=i;}
-  for(i=0;i<13;i++)for(j=0;j<13;j++)mask[i][j]=map[i][j]=='.'?0:99;
-  mask[i][j]++;
-  for(;;)
+  return x<0||y<0||x>=r||y>=c||visit[t][x][y]||map[x][y]!='.'?0:1;
+}
+f(x,y,t1,t2)
+{
+  if(end)return;
+  visit[t1][x][y]++;
+  if(count==0 && x==sx && y==sy){end=1;return;}
+  count--;
+  int res=0;
+  if(isOK(x-1,y,3) && t1!=1 && t2!=1){res++;f(x-1,y,3,t1);}
+  if(isOK(x,y-1,4) && t1!=2 && t2!=2){res++;f(x,y-1,4,t1);}
+  if(isOK(x+1,y,1) && t1!=3 && t2!=3){res++;f(x+1,y,1,t1);}
+  if(isOK(x,y+1,2) && t1!=4 && t2!=4){res++;f(x,y+1,2,t1);}
+  if(res == 0)end=2;
+}
+
+main()
+{
+  int i,j;
+  
+  scanf("%d%d ",&r,&c);
+  for(i=0;i<r;i++)
   {
-    
-    sel=min(min(min(mask[y-1][x],mask[y+1][x]),mask[y][x-1]),mask[y][x+1]);
-    printf("%d %d %d %d %d\n",x,y,sel,now,past);
-    if(mask[y-1][x]==sel&&now!=DOWN&&!((now==LEFT||now==RIGHT)&&past==DOWN))
-    {
-      past=now;
-      now=UP;
-      y--;
-    }
-    else if(mask[y+1][x]==sel&&now!=UP&&!((now==LEFT||now==RIGHT)&&past==UP))
-    {
-      past=now;
-      now=DOWN;
-      y++;
-    }
-    else if(mask[y][x-1]==sel&&now!=RIGHT&&!((now==UP||now==DOWN)&&past==RIGHT))
-    {
-      past=now;
-      now=LEFT;
-      x--;
-    }
-    else if(mask[y][x+1]==sel&&now!=LEFT&&!((now==UP||now==DOWN)&&past==LEFT))
-    {
-      past=now;
-      now=RIGHT;
-      x++;
-    }
-    else
-    {
-      puts("1");
-      while(1);
-      return;
-    }
-    
-    mask[y][x]++;
-    k=0;for(i=1;i<=R;i++)for(j=1;j<=C;j++)k+=mask[i][j]!=99&&mask[i][j]>0?1:0;
-    if(k==m){puts("0");return;}
+    gets(map[i]);
+    for(j=0;map[i][j];j++)if(map[i][j]=='.')numLoad++;
   }
+  
+  for(i=0;i<r;i++)
+  {
+    for(j=0;j<c;j++)
+    {
+      if(map[i][j]=='.')
+      {
+        memset(visit,0,sizeof(visit));
+        count=numLoad*4;
+        end=0;
+        sx=i;
+        sy=j;
+        f(i,j,0,0);
+        if(end!=1)return puts("1"),0;
+      }
+    }
+  }
+  puts("0");
 }
