222f10caf2c387ea02c0ce909a9e08d283c34fb7
Yukariko <rkdals7979@naver.com>
2014-10-14 23:26:12 +0900

    Accept

--- problem/2086/test.c
+++ problem/2086/test.c
@@ -1,28 +1,58 @@
-//a^b mod c
-double p,q;
-double mod(double a,long long c)
+#define MOD 1000000000
+//sum f(a) to f(b) => f(b+2) - f(a+1)
+typedef struct matrix{
+  long long a,b,c,d;
+} matrix;
+
+matrix mod_matrix(matrix m,long long c)
 {
-  double t=a>0?a:-a;
-  for(;t>=c;)t-=c;
-  return a>0?t:-t;
+  m.a%=c;
+  m.b%=c;
+  m.c%=c;
+  m.d%=c;
+  return m;
 }
-double mod_exp(double a,long long b,long long c)
+
+matrix mult_matrix(matrix p,matrix q)
 {
-  puts("!");
-  a=mod(a,c);
-  if(b==0)return 1;
-  if(b==1)return a;
-  if(b&1) return mod((a*mod_exp(mod(a*a,c),(b-1)/2,c)),c);
-  return mod_exp(mod(a*a,c),b/2,c);
+  matrix res;
+  res.a = p.a * q.a + p.b * q.c;
+  res.b = p.a * q.b + p.b * q.d;
+  res.c = p.c * q.a + p.d * q.c;
+  res.d = p.c * q.b + p.d * q.d;
+  return res;
 }
+
+// m ^ b mod c
+matrix greatPow(matrix m,long long b, long long c)
+{
+  m=mod_matrix(m,c);
+  if(b==0)
+  {
+    m.a=m.b=m.c=m.d=0;
+    return m;
+  }
+  if(b==1) return m;
+  if(b&1) return mod_matrix(mult_matrix(m,greatPow(mult_matrix(m,m),(b-1)/2,c)),c);
+  return greatPow(mult_matrix(m,m),b/2,c);
+}
+
+long long fibo(long long n)
+{
+  long long res;
+  matrix m;
+  m.a=1;m.b=1;m.c=1;m.d=0;
+  if(n==1 || n==2) return 1;
+  m = greatPow(m,n-2,MOD);
+  res = (m.a + m.b)%MOD;
+  return res;
+}
+
 main()
 {
-  long long a,b,c=1000000000;
+  long long a,b,res;
   scanf("%lld%lld",&a,&b);
-  p=(1.0+sqrt(5.0))/2.0;
-  q=(1.0-sqrt(5.0))/2.0;
-  a=(mod_exp(p,a+1,c)-mod_exp(q,a+1,c))/sqrt(5.0)+1e-6 - 2;
-  puts("!");
-  b=(mod_exp(p,b+2,c)-mod_exp(q,b+2,c))/sqrt(5.0)+1e-6 - 2;
-  printf("%lld",b-a);
+  res = fibo(b+2)-fibo(a+1);
+  if(res < 0) res += MOD;
+  printf("%lld",res);
 }
