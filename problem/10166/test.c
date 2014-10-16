f831109e6ed960262095ebd05e68b1f6f7474bb9
Yukariko <rkdals7979@naver.com>
2014-10-16 17:27:20 +0900

    Accept

---
+++ problem/10166/test.c
@@ -0,0 +1,21 @@
+main()
+{
+  char angle[4000021]={};
+  int d1,d2;
+  int i,sel,res=0;
+  scanf("%d%d",&d1,&d2);
+  for(;d1<=d2;d1++)
+  {
+    
+    for(i=0;i<d1;i++)
+    {
+      sel=4000020LL*i/d1;
+      if(angle[sel]==0)
+      {
+      	res++;
+      	angle[sel]=1;
+      }
+    }
+  }
+  printf("%d",res);
+}
