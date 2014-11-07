83919da9071248643baf52659194c3990f83bfcd
Yukariko <rkdals7979@naver.com>
2014-11-07 19:50:00 +0900

    Accept

---
+++ problem/1662/test.c
@@ -0,0 +1,22 @@
+main()
+{
+  char S[51];
+  int stk[51]={};
+  int mult[51];
+  int sp;
+  int i;
+  
+  gets(S);
+  
+  for(sp=i=0;S[i];i++)
+  {
+    if(S[i] == '('){stk[sp]--; sp++; mult[sp] = S[i-1] - '0'; }
+    else if(S[i] == ')'){stk[sp-1] += mult[sp] * stk[sp]; stk[sp] = 0; sp--;}
+    else
+    {
+      stk[sp]++;
+    }
+  }
+  for(i=1;i<4;i++)printf("%d ",mult[i]);
+  printf("%d",stk[0]);
+}
