284b10db4808f1f0fff4ea2c477c512d8c9c8312
Yukariko <rkdals7979@naver.com>
2015-01-29 12:15:42 +0900

    Accept

---
+++ problem/10426/test.c
@@ -0,0 +1,29 @@
+const int DAY[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+int leap(int x){return x%4?0:x%100?1:x%400?0:1;}
+
+main()
+{
+	int N;
+	int year, month, day;
+	scanf("%d-%d-%d %d",&year,&month,&day,&N);
+	N--;
+	for(;N;)
+	{
+		int t = month == 2 && leap(year)?29: DAY[month];
+		t -= day - 1;
+		if(N < t)
+		{
+			day += N;
+			break;
+		}
+		N -= t;
+		month++;
+		if(month == 13)
+		{
+			month = 1;
+			year++;
+		}
+		day = 1;
+	}
+	printf("%.4d-%.2d-%.2d",year,month,day);
+}
