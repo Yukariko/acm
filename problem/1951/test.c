0e06aa0dd5e4c9b5902c39756c6630713cc47ef7
Yukariko <rkdals7979@naver.com>
2015-01-29 11:06:44 +0900

    Accept

---
+++ problem/1951/test.c
@@ -0,0 +1,32 @@
+long long digitCount(long long n,int i)
+{
+  long long res,j,k,t;
+  res=0;
+  for(t=0,j=1;j<=n;j*=10,t++)
+  {
+    k=(n/j)%10;
+    if(i==0)
+    {
+      if(!k)
+      {
+        res-=j-1;
+        res+=n%j;
+      }
+    }
+    else if(i<k)res+=j;
+    else if(i==k)res+=n%j+1;
+    res+=k*t*(j/10);
+  }
+  return res;
+}
+main()
+{
+	int N;
+	scanf("%d",&N);
+	long long ret = 0;
+	for(int i=0;i<10;i++)
+	{
+		ret += digitCount(N,i);
+	}
+	printf("%lld",ret%1234567);
+}
