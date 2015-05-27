088e5dcc9ff07ebcc932661d3a9a6025ac326a82
Yukariko <rkdals7979@naver.com>
2015-05-27 22:05:20 +0900

    Accept

--- problem/1023/test.c
+++ problem/1023/test.c
@@ -1,79 +1,62 @@
-#include <math.h>
-long long i,k,a;
-n,j,p;
-f(r)
+#include <stdio.h>
+
+int N;
+long long K;
+long long dp[51][51];
+char res[51];
+
+long long solve(int pos,int left)
 {
-	if (i & 1 == 0)
-	  return 1;
-	p = log2(i) + 1e-6 + 1.0;
-	r = n - p;
-  if (!r) 
-      return 1;
-	if (r > n / 2)
-       return 1;
-	for (j = p-1; j >= 0 && r >= 0 && r <= n/2; j--)
+	if(pos == N)
 	{
-		if ((i >> j) % 2 == 1)
-			r--;
-		else
-			r++;
+		if(left == 0)
+			return 0;
+		if(K-- == 0)
+		{
+			puts(res);
+			exit(1);
+		}
+		return 1;
 	}
-		return r;
+
+	long long ret = 0;
+
+	if(left >= 0)
+	{
+		long long *ret2 = &dp[pos][left];
+		if(*ret2 != -1 && *ret2 < K)
+		{
+			K -= *ret2;
+			return *ret2;
+		}
+		*ret2 = 0;
+		res[pos] = '(';
+		*ret2 += solve(pos+1,left+1);
+		res[pos] = ')';
+		*ret2 += solve(pos+1,left-1);
+		ret = *ret2;
+	}
+	else
+	{
+		if((1LL << (N-pos)) < K)
+		{
+			K -= (1LL << (N-pos));
+			return (1LL << (N-pos));
+		}
+		res[pos] = '(';
+		ret += solve(pos+1,left);
+		res[pos] = ')';
+		ret += solve(pos+1,left);	
+	}
+	return ret;
 }
-main()
+
+
+int main()
 {
-    long long r,t=0;
-	scanf("%d%lld", &n, &k);
-	a = ((long long)1 << n);
-  for(k=0;k<a;k++)
-  {
-    i=r=j=p=0;
-  	if(n%2==0)
-  	{
-      i = ((long long)1 << (n / 2)) - 1;
-      if ( i > k)
-        i = k;
-      else {
-        for (r=0; i < a; i++)
-        {
-          if (!f(0))
-          {
-              r++;
-          }
-          if (i - r == k)
-          {     
-              break;
-          }
-        }
-      }
-   	}
-   	else
-   	{
-    	i = k;
-    }
-  	if (i == a)
-  	{
-  	  t++;
-  	   printf("%d",t);
-  	   puts("");
-  	 }
-    else
-    {
-     // printf("[%d] ",k);
-      for (r=n; r--;)
-      {
-        if ((i >> r) % 2 == 1)
-        {
-         // printf("1");
-        }
-        else
-        {
-         // printf("0");
-        }
-      }
-    //puts("");
-    }
-
-  }
+	memset(dp,-1,sizeof(dp));
+	scanf("%d%lld",&N,&K);
+	solve(0,0);
+	puts("-1");
+	return 0;
 }
-
