f2781973517a66aac4838192c2d306040be1618b
Yukariko <rkdals7979@naver.com>
2015-06-16 16:54:31 +0900

    Accept

--- problem/2805/test.c
+++ problem/2805/test.c
@@ -1,41 +1,43 @@
+#include <stdio.h>
+
 int a[1000001];
-main()
+
+int main()
 {
   int n,m;
-  scanf("%d%d",&n,&m);
-  int i;
   int max,min;
+  int mid,s;
+  scanf("%d%d",&n,&m);
   scanf("%d",a);
+
   max=min=a[0];
-  for(i=1;i<n;i++)
+  for(int i=1;i<n;i++)
   {
-    scanf("%d",a+i);
+    scanf("%d",&a[i]);
     if(max<a[i])
-    {
       max=a[i];
-    }
     else if(min>a[i])
-    {
       min=a[i];
-    }
   }
-  int mid;
-  int res=m,sel;
-  for(;min<=max;)
+  
+  while(min<=max)
   {
     mid=(max+min)/2;
-    long long s=0;
-    for(i=0;i<n;i++)if(mid<a[i])s+=a[i]-mid;
-    if(s>=m)
-    {
-      min=mid+1;
-      if(res>s-m)
-      {
-        res=s-m;
-        sel=mid;
-      }
-    }
-    else if(s<m)max=mid-1;
+    s=0;
+    for(int i=0;i<n && s <m ;i++)
+      if(a[i]>mid)
+        s+=a[i]-mid;
+        
+    if(s<m)
+			max=mid-1;
+    else if(s>m)
+			min=mid+1;
+    else
+		{
+			max=mid;
+			break;
+		}
   }
-  printf("%d",sel);
+  printf("%d",max);
 }
+
