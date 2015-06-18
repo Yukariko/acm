bb363305fd6fd54eebd965be0d839135c0c9b272
Yukariko <rkdals7979@naver.com>
2015-06-18 16:33:04 +0900

    Accept

--- problem/2178/test.c
+++ problem/2178/test.c
@@ -1,92 +1,95 @@
-#define MAX_QUEUE 1
-typedef struct coord{int x,y;} DATA;
-typedef struct queue{DATA data;struct queue *next;}queue;
-queue *head[MAX_QUEUE],*tail[MAX_QUEUE];
-void push(int index,DATA val)
+#include <stdio.h>
+
+typedef struct coord{
+	int x,y;
+} DATA;
+
+typedef struct queue{
+	DATA data;
+	struct queue *next;
+} Queue;
+
+Queue *head,*tail;
+
+void push(DATA val)
 {
-  tail[index]->data=val;
-  tail[index]->next=(queue *)malloc(sizeof(queue));
-  tail[index]=tail[index]->next;
-  tail[index]->next=0;
+  tail->data=val;
+  tail->next=(Queue *)malloc(sizeof(Queue));
+  tail=tail->next;
+  tail->next=0;
 }
-DATA pop(int index)
+DATA pop()
 {
-  queue *p=head[index];
-  DATA result=head[index]->data;
-  head[index]=head[index]->next;
+  Queue *p=head;
+  DATA result=head->data;
+  head=head->next;
   free(p);
   return result;
 }
-int isEmpty(int index)
+int isEmpty()
 {
-  return !head[index];
+  return !head;
 }
-void init(int index)
+void init()
 {
-  head[index]=(queue *)malloc(sizeof(queue));
-  head[index]->next=0;
-  tail[index]=head[index];
-}
-void destroy(int index)
-{
-  for(;!isEmpty(index);)pop(index);
+  head=(Queue *)malloc(sizeof(Queue));
+  head->next=0;
+  tail=head;
 }
 
-main()
+const int D[2][4] = {
+	{0,0,1,-1},
+	{1,-1,0,0}
+};
+
+int main()
 {
-  char map[101][101];
-  int n,m;
-  int i,j;
-  init(0);
-  scanf("%d%d",&n,&m);
-  for(i=1;i<=n;i++)
-  {
-    for(j=1;j<=m;j++)
-    {
-      scanf(" %c",&map[i][j]);
-      map[i][j]-=48;
-    }
-  }
-  DATA t,pick;
-  int s,k,p;
-  t.x=1;t.y=1;
-  push(0,t);
-  k=1;
-  for(s=1;;s++)
-  {
-    for(p=k,k=0;p--;)
-    {
-      pick=pop(0);
-      t.x=t.y=0;
-      if(pick.x==n&&pick.y==m){printf("%d",s);return;}
-      if(pick.x>1&&map[pick.x-1][pick.y])
-      {
-        map[pick.x-1][pick.y]=0;
-        t.x=pick.x-1;t.y=pick.y;
-        push(0,t);
-        k++;
-      }
-      if(pick.x<n&&map[pick.x+1][pick.y])
-      {
-        map[pick.x+1][pick.y]=0;
-        t.x=pick.x+1;t.y=pick.y;
-        push(0,t);
-        k++;
-      }
-      if(pick.y>1&&map[pick.x][pick.y-1])
-      {
-        map[pick.x][pick.y-1]=0;
-        t.x=pick.x;t.y=pick.y-1;
-        push(0,t);
-        k++;
-      }
-      if(pick.y<m&&map[pick.x][pick.y+1])
-      {
-        map[pick.x][pick.y+1]=0;
-        t.x=pick.x;t.y=pick.y+1;
-        push(0,t);
-        k++;
-      }
-    }
-  }
+	char map[101][101];
+	int N,M;
+	
+	init();
+	scanf("%d%d",&N,&M);
+	
+	for(int i=1;i<=N;i++)
+	{
+		for(int j=1;j<=M;j++)
+		{
+			scanf(" %c",&map[i][j]);
+			map[i][j]-=48;
+		}
+	}
+	
+	DATA t;
+	t.x=1;t.y=1;
+	push(t);
+	
+	for(int s=1,k=1;;s++)
+	{
+		int p = k;
+		k = 0;
+		while(p--)
+		{
+			DATA pick=pop();
+			if(pick.x == N && pick.y == M)
+			{
+				printf("%d",s);
+				return 0;
+			}
+			
+			for(int i=0;i<4;i++)
+			{
+				int new_x = pick.x+D[0][i];
+				int new_y = pick.y+D[1][i];
+				
+				if(new_x < 1 || new_y < 1 || new_x > N || new_y > M || !map[new_x][new_y])
+					continue;
+				
+				map[new_x][new_y]=0;
+				t.x=new_x;
+				t.y=new_y;
+				push(t);
+				k++;
+			}
+		}
+	}
 }
