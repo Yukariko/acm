8b328a8d9758767c9cb1f28a670707c0d4b6e4d6
Yukariko <rkdals7979@naver.com>
2015-06-18 16:16:53 +0900

    Accept

--- problem/1181/test.c
+++ problem/1181/test.c
@@ -1,12 +1,40 @@
-struct WORDBOOK{int len;char word[51];}wordbook[20001];
-int i,temp;
-cmp(struct WORDBOOK *a,struct WORDBOOK *b)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct WORDBOOK{
+	int len;
+	char word[51];
+};
+
+struct WORDBOOK wb[20001];
+
+int cmp(struct WORDBOOK *a,struct WORDBOOK *b)
 {
-  return a->len > b->len?1:a->len < b->len?-1:strcmp(a->word,b->word);
+	return a->len > b->len?1:a->len < b->len?-1:strcmp(a->word,b->word);
 }
-main(n)
+
+int main()
 {
-  for(scanf("%d ",&n);gets(wordbook[i].word);++i)wordbook[i].len=strlen(wordbook[i].word);
-  qsort(wordbook,n,sizeof(struct WORDBOOK),cmp);
-  for(i=1;i<=n;i++)if(strcmp(wordbook[temp].word,wordbook[i].word)){puts(wordbook[temp].word);temp=i;}
+	int N;
+	scanf("%d ",&N);
+	for(int i=0;i<N;i++)
+	{
+		gets(wb[i].word);
+		wb[i].len=strlen(wb[i].word);
+	}
+	
+	qsort(wb,N,sizeof(struct WORDBOOK),cmp);
+	
+	int temp = 0;
+	for(int i=1;i<=N;i++)
+	{
+		if(strcmp(wb[temp].word,wb[i].word))
+		{
+			puts(wb[temp].word);
+			temp=i;
+		}
+	}
+	
+	return 0;
 }
