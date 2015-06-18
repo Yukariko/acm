da41c9d9f3b006530f61f0f9e499730f56910449
Yukariko <rkdals7979@naver.com>
2015-06-18 16:09:47 +0900

    Acceot

--- problem/9465/test.c
+++ problem/9465/test.c
@@ -1,26 +1,31 @@
+#include <stdio.h>
+
 #define MAX(a,b) (a>b?a:b)
-main()
+
+int main()
 {
   int t;
-  int a[100001];
+  int a[2][100001];
+  int dp[2][100001];
+  
   for(scanf("%d",&t);t--;)
   {
     int n;
     scanf("%d",&n);
-    int i,j;
-    int x,y;
-    int tx,ty;
-    for(i=0;i<n;i++)scanf("%d",a+i);
-    x=a[0];
-    scanf("%d",&y);
-    for(i=1;i<n;i++)
+    
+    for(int j=0;j<2;j++)
+			for(int i=0;i<n;i++)
+				scanf("%d",&a[j][i]);
+    
+		dp[0][0]=a[0][0];
+    dp[1][0]=a[1][0];
+    
+    for(int i=1;i<n;i++)
     {
-      scanf("%d",&j);
-      tx=MAX(x,y+a[i]);
-      ty=MAX(y,j+x);
-      x=tx;
-      y=ty;
+      dp[0][i]=MAX(dp[0][i-1],a[0][i]+dp[1][i-1]);
+      dp[1][i]=MAX(dp[1][i-1],a[1][i]+dp[0][i-1]);
     }
-    printf("%d\n",MAX(x,y));
+    printf("%d\n",MAX(dp[1][n-1],dp[0][n-1]));
   }
+  return 0;
 }
