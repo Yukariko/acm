86c9750dbc3246826b6ac7a31c908936d340c94e
Yukariko <rkdals7979@naver.com>
2015-06-18 16:10:48 +0900

    Accept

--- problem/9465/test.c
+++ problem/9465/test.c
@@ -4,28 +4,28 @@
 
 int main()
 {
-  int t;
-  int a[2][100001];
-  int dp[2][100001];
-  
-  for(scanf("%d",&t);t--;)
-  {
-    int n;
-    scanf("%d",&n);
-    
-    for(int j=0;j<2;j++)
+	int t;
+	int a[2][100001];
+	int dp[2][100001];
+	
+	for(scanf("%d",&t);t--;)
+	{
+		int n;
+		scanf("%d",&n);
+	
+		for(int j=0;j<2;j++)
 			for(int i=0;i<n;i++)
 				scanf("%d",&a[j][i]);
-    
+		
 		dp[0][0]=a[0][0];
-    dp[1][0]=a[1][0];
-    
-    for(int i=1;i<n;i++)
-    {
-      dp[0][i]=MAX(dp[0][i-1],a[0][i]+dp[1][i-1]);
-      dp[1][i]=MAX(dp[1][i-1],a[1][i]+dp[0][i-1]);
-    }
-    printf("%d\n",MAX(dp[1][n-1],dp[0][n-1]));
-  }
-  return 0;
+		dp[1][0]=a[1][0];
+		
+		for(int i=1;i<n;i++)
+		{
+			dp[0][i]=MAX(dp[0][i-1],a[0][i]+dp[1][i-1]);
+			dp[1][i]=MAX(dp[1][i-1],a[1][i]+dp[0][i-1]);
+		}
+		printf("%d\n",MAX(dp[1][n-1],dp[0][n-1]));
+	}
+	return 0;
 }
