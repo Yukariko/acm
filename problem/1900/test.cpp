817b3f2742265e191ddf2fae3fbd02929e2a3869
Yukariko <rkdals7979@naver.com>
2015-07-13 01:44:27 +0900

    Accept

---
+++ problem/1900/test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct player
+{
+	int no, pow, mag;
+};
+
+bool cmp(const player &p, const player &q)
+{
+	int a = p.pow + q.pow * p.mag;
+	int b = q.pow + p.pow * q.mag;
+	return a > b;
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	player a[10001];
+	for(int i=0; i < N; i++)
+	{
+		a[i].no = i+1;
+		cin >> a[i].pow >> a[i].mag;
+	}
+
+	sort(a, a+N, cmp);
+
+	for(int i=0; i < N; i++)
+		cout << a[i].no << endl;
+
+	return 0;
+}
