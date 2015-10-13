9567016f77d889261f91daa90f2482b957b83690
Yukariko <rkdals7979@naver.com>
2015-10-13 18:52:58 +0900

    Acceot

---
+++ problem/1398/test.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int T;
+	scanf("%d",&T);
+
+	int div[] = {1, 10, 25};
+
+	while(T--)
+	{
+		long long N;
+		scanf("%lld",&N);
+
+		long long ans = 0;
+		while(N)
+		{
+			int dp[101];
+			dp[0] = 0;
+			for(int i=1; i <= 100; i++)
+				dp[i] = 987654321;
+
+			for(int i=0; i < 3; i++)
+				for(int j=div[i]; j <= N % 100; j++)
+					dp[j] = min(dp[j], dp[j-div[i]] + 1);
+			ans += dp[N % 100];
+			N /= 100;
+		}
+		printf("%lld\n", ans);
+	}
+	return 0;
+}
