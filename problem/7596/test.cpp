4076f4f6387e03c3d4799dd57b159c2a15e7469f
Yukariko <rkdals7979@naver.com>
2016-05-31 05:07:00 +0900

    string

---
+++ problem/7596/test.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	for(int tc=1; scanf("%d ", &N), N; tc++)
+	{
+		printf("%d\n", tc);
+
+		vector<string> a(N);
+		for(int i=0; i < N; i++)
+		{
+			char b[256];
+			gets(b);
+			a[i] = b;
+		}
+
+		sort(a.begin(), a.end());
+
+		for(int i=0; i < N; i++)
+			puts(a[i].c_str());
+	}
+	return 0;
+}
